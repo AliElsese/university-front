84058a73afbddfc6f78af62af73de1d3fe53dde3
AliElsese <ali.elsese104@gmail.com>
2025-05-18 12:07:20 +0300

    removing dark

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useEffect } from "react";
 import Router from "./Router";
 export default function App() {
   // Enable Dark Mode
-  const loadTheme = () => {
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'dark') {
-      document.documentElement.classList.add('dark');
-    }
-  }
+  // const loadTheme = () => {
+  //   const savedTheme = localStorage.getItem('theme');
+  //   if (savedTheme === 'dark') {
+  //     document.documentElement.classList.add('dark');
+  //   }
+  // }
   
   return (
     <Router />
